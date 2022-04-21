5855a38a5b67633c58c6c2bb63b6aeeb181d01e1
bangdler <zbthz90@gmail.com>
2022-04-22 00:27:47 +0900

    baekjoon no.1351

---
+++ baekjoon/no.1351.js
@@ -0,0 +1,30 @@
+function getInput() {
+    const fs = require('fs');
+    const path = require('path');
+
+    const numOfProblem = 1351;
+    const filePath =
+        process.platform === 'linux'
+            ? '/dev/stdin'
+            : path.join(__dirname, `/test/${numOfProblem}.txt`);
+
+    const input = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+    return input;
+}
+
+function run() {
+    const [N, P, Q] = getInput()
+
+    let dp = { 0: 1, 1: 2 }
+
+    console.log(solve(N))
+
+    function solve(n) {
+        if (dp[n]) return dp[n]
+        const left = solve(Math.floor(n / P))
+        const right = solve(Math.floor(n / Q))
+        return dp[n] = left + right
+    }
+}
+
+run()
