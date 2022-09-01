5126fd58c7d6d8961bf041dcb009c78b03ef786b
bangdler <zbthz90@gmail.com>
2022-09-01 17:33:59 +0900

    폴더 구조 변경, udemy_recursion bonus 문제

---
+++ studyPractice/recursion/collectStrings.js
@@ -0,0 +1,42 @@
+// Write a function called collectStrings which accepts an object and
+// returns an array of all the values in the object that have a typeof string
+
+function collectStrings(obj) {
+    if(!Object.keys(obj).length) return
+    const strings = []
+    for(let key in obj) {
+        const value = obj[key]
+        if(typeof value === 'string') {
+            strings.push(value)
+        }
+        else if(typeof value === 'object' && !Array.isArray(value)) {
+            return strings.concat(collectStrings(value))
+        }
+    }
+    return strings
+}
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+const obj2 = {
+    a : 'a',
+    b : {
+        c: 'c'
+    }
+}
+
+console.log(collectStrings(obj))// ["foo", "bar", "baz"])
