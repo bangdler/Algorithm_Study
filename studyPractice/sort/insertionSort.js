40ff7149c3de08639e94e3d52fa243abb7759cba
bangdler <zbthz90@gmail.com>
2022-09-06 19:10:07 +0900

    udemy_insertion sort

---
+++ studyPractice/sort/insertionSort.js
@@ -0,0 +1,39 @@
+
+// 시간복잡도 O(N^2)
+// 효율은 그닥, 정렬된 상태에서 데이터가 추가되는 경우 추가되는 데이터를 정렬시킬 때 쓸 수 있다.
+
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        let curValue = array[i]
+        // 정렬된 배열의 오른쪽부터 순회. 현재값보다 정렬배열의 값이 크면 정렬배열의 값을 오른쪽으로 바꿔준다.
+        let j;
+        for(j = i-1; j >= 0 && array[j] > curValue; j--) {
+            console.log('위치 이동', array, array[j], "->", array[j+1])
+            array[j+1] = array[j] // array[j] 정려배열 값을 오른쪽으로 이동
+            console.log('이동 완료', array)
+        }
+        console.log('마지막 바꾸기', array, array[j+1], "<-", curValue)
+        array[j+1] = curValue // 현재값이 들어갈 위치는 array[j] < curValue 인 위치의 다음.
+        console.log('one cycle',array)
+    }
+    return array
+}
+function insertionSort_my(array) {
+    for(let i = 1; i < array.length; i++) {
+        for(let j = 0; j < i; j++) {
+            console.log('compare', array[i], array[j])
+            if(array[i] < array[j]) {
+                let temp = array[i]
+                array.splice(i,1) // 정렬되지 않은 배열에서 제거
+                console.log('삭제', array)
+                array.splice(j,0,temp) // 정렬된 배열에 삽입
+                console.log('삽입', array)
+                break;
+            }
+        }
+        console.log('one cycle!', array)
+    }
+    return array
+}
+
+insertionSort([1,10,3,2,4])
