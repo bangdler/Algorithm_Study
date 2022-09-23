2bacf443f4d3ed91438a21a1c44cdb99652f2057
bangdler <zbthz90@gmail.com>
2022-09-24 01:54:42 +0900

    udemy_binaryHeap 주석 추가, priorityQueue 구현

--- studyPractice/dataStructure/binaryHeap.js
+++ studyPractice/dataStructure/binaryHeap.js
@@ -1,11 +1,15 @@
-// binary tree 와 다르게 부모 요소를 기준으로 자식 요소는 동등하다. 2개 모두 채워야 다음 depth 로 간다.
-// max binary heap : 자식요소는 부모요소보다 작다.
-// min binary heap : 자식요소는 부모요소보다 크다.
+// binary tree 와 다르게 부모 요소를 기준으로 자식 요소는 동등하다.
+// 자식요소 2개 모두 채워야 다음 depth 로 간다. -> 비정상 구조 tree 를 만들지 않는다.
+// max binary heap : 부모요소는 항상 자식요소보다 크다.
+// min binary heap : 부모요소는 항상 자식요소보다 작다.
 // 힙을 배열로 늘어뜨려 정렬했을 때, 한 요소의 자식요소를 찾는 방법은 아래와 같다
 // 부모요소 index 가 n 일 때, left child index: 2n+1, right child index : 2n+2
 // 거꾸로 하면 한 요소의 부모요소도 찾을 수 있다. 자식요소 index 가 n 일 때, 부모요소 index : Math.floor(n-1/2)
 // https://cs.slides.com/colt_steele/heaps#/18
 
+// 시간복잡도 : insert / remove 모두 O(lonN), search O(N)
+// insert : n 이 2배가 되어도 부모 요소와 비교하는 횟수는 1번 증가한다.
+// remove : n 이 2배가 되어도 자식 요소와 비교하는 횟수는 1번 증가한다.
 class MaxBinaryHeap {
   constructor() {
     this.values = [];
@@ -53,6 +57,7 @@ class MaxBinaryHeap {
     return max;
   }
 
+  // 초기 version
   sinkDown1() {
     let currentIdx = 0;
     const current = this.values[currentIdx];
@@ -90,9 +95,9 @@ class MaxBinaryHeap {
       const rightIdx = 2 * currentIdx + 2;
       const left = this.values[leftIdx];
       const right = this.values[rightIdx];
-      // right 가 undefined 인 경우가 있다.
+      // maxCurrentIdx 는 left 의 존재만 보장하기 때문에 right 가 undefined 인 경우가 있다.
       // 바꿀 자식요소(=가장 큰 값)에 해당하는 swapIndex 를 먼저 지정하고 swap 한다.
-      const swapIdx = right ? (left >= right ? leftIdx : rightIdx) : leftIdx;
+      const swapIdx = right ? (left > right ? leftIdx : rightIdx) : leftIdx;
       const largestChild = this.values[swapIdx];
       if (current >= largestChild) break;
       this.values[swapIdx] = current;
---
+++ studyPractice/dataStructure/priorityQueue.js
@@ -0,0 +1,89 @@
+// priority queue 는 우선순위가 높은(숫자가 낮은) 것부터 처리해야한다.
+// 일반적으로 높거나 낮은 값부터 구성된 heap 을 활용한다.
+
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+// minBinaryHeap 과 거의 동일함.
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  // node 삽입, 제거 시 같은 우선순위는 별다른 구분이 없기 때문에 삽입 순서가 보장되지 않는다.
+  // 삽입 순서를 보장하기 위해서는 같은 우선순위 비교를 위한 insertTime 같은 변수를 만들어야 함.
+  enqueue(value, priority) {
+    const newNode = new Node(value, priority);
+    this.queue.push(newNode);
+    this.bubbleUp();
+    return this.queue;
+  }
+
+  bubbleUp() {
+    let currentIdx = this.queue.length - 1;
+    const current = this.queue[currentIdx];
+
+    while (currentIdx > 0) {
+      const parentIdx = Math.floor((currentIdx - 1) / 2);
+      const parent = this.queue[parentIdx];
+      if (current.priority >= parent.priority) break;
+      this.queue[currentIdx] = parent;
+      this.queue[parentIdx] = current;
+      currentIdx = parentIdx;
+    }
+  }
+
+  // min 값을 반환
+  dequeue() {
+    if (!this.queue.length) return undefined;
+    if (this.queue.length === 1) return this.queue.pop();
+
+    const min = this.queue[0];
+    const end = this.queue.pop();
+    this.queue[0] = end;
+
+    this.sinkDown();
+
+    return min;
+  }
+
+  sinkDown() {
+    let currentIdx = 0;
+    const current = this.queue[currentIdx];
+    const maxCurrentIdx = Math.floor((this.queue.length - 2) / 2);
+
+    while (currentIdx <= maxCurrentIdx) {
+      const leftIdx = currentIdx * 2 + 1;
+      const rightIdx = currentIdx * 2 + 2;
+      const left = this.queue[leftIdx];
+      const right = this.queue[rightIdx];
+      const swapIdx = right ? (left.priority < right.priority ? leftIdx : rightIdx) : leftIdx;
+      const minChild = this.queue[swapIdx];
+
+      if (current.priority < minChild.priority) break;
+      this.queue[currentIdx] = minChild;
+      this.queue[swapIdx] = current;
+      currentIdx = swapIdx;
+    }
+  }
+}
+
+const pQue = new PriorityQueue();
+
+console.log(pQue.enqueue('eat', 3));
+console.log(pQue.enqueue('run', 5));
+console.log(pQue.enqueue('jump', 5));
+console.log(pQue.enqueue('sleep', 1));
+console.log(pQue.enqueue('drink', 4));
+console.log(pQue.enqueue('sing', 4));
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
+console.log(pQue.dequeue());
