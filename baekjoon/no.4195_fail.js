de1bd5640812443b1ae89860a97592c2a7355ca1
bangdler <zbthz90@gmail.com>
2022-11-02 17:07:15 +0900

    baekjoon 4195 다른 풀이- 실패

--- baekjoon/no.4195_fail.js
+++ baekjoon/no.4195_fail.js
@@ -14,6 +14,60 @@ function getInput() {
   return input;
 }
 
+// 그래프를 사용한 탐색 - 메모리 초과ㅠ
+function run2() {
+  const [[T], ...arr] = getInput();
+  const answer = [];
+
+  for (let i = 0; i < Number(T); i++) {
+    const [relationshipNum] = arr.splice(0, 1).flat();
+    const relationshipArr = arr.splice(0, Number(relationshipNum));
+
+    const network = {};
+    relationshipArr.forEach(list => {
+      const [a, b] = list;
+      // add vertex
+      if (!network[a]) {
+        network[a] = [];
+      }
+      if (!network[b]) {
+        network[b] = [];
+      }
+
+      // add edge
+      network[a].push(b);
+      network[b].push(a);
+
+      // dfs
+      const networkPaths = dfs(network, a);
+
+      answer.push(networkPaths.length);
+    });
+  }
+
+  console.log(answer.join('\n'));
+}
+
+function dfs(network, start) {
+  const visited = {};
+  const result = [];
+  visited[start] = true;
+  const stack = [start];
+
+  while (stack.length) {
+    const cur = stack.pop();
+    result.push(cur);
+    network[cur].forEach(neighbor => {
+      if (visited[neighbor]) return;
+      visited[neighbor] = true;
+      stack.push(neighbor);
+    });
+  }
+
+  return result;
+}
+
+// 실패한 방법
 function run() {
   const [[T], ...arr] = getInput();
   let answer = [];
@@ -44,4 +98,4 @@ function run() {
   console.log(answer.join('\n'));
 }
 
-run();
+run2();
